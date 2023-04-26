517a0f6f98cbfb4884560833d61a8df92ee77f07
florent <florentdekadjevi04@gmail.com>
2023-04-26 10:05:03 +0000

    premier commit

---
+++ exercice.java
@@ -0,0 +1,14 @@
+public class Maclasse exends thread{
+    private static int compteur;
+    publicvoid run(){
+        compteur=compteur+1;
+        System;out.println(compteur);
+
+    }
+
+    public static void main(String[]args){
+        Maclasset1=new Maclasse;
+        Maclasset2=new=Maclasse();
+        t1.start();t2.start();
+    }
+}
